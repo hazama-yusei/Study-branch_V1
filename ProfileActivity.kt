396421d353421408601c6ca148a3e5f916fac480
yusei hazama <yusei.hazam@xacti-co.com>
2026-09-24 13:47:52 +0900

    ProfileActivity一回コミット

---
+++ ProfileActivity.kt
@@ -0,0 +1,18 @@
+class ProfileActivity {
+
+    fun onCreate() {
+        println("ProfileActivity created")
+    }
+
+    fun loadProfile(userName: String) {
+        println("Loading profile: $userName")
+    }
+
+    fun updateProfile(userName: String) {
+        println("Profile updated: $userName")
+    }
+
+    fun onDestroy() {
+        println("ProfileActivity destroyed")
+    }
+}
