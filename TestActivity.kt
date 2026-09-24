34e0eacb5836dac77dac2e8bd59c7ba663d86f6d
yusei hazama <yusei.hazam@xacti-co.com>
2026-09-24 11:28:35 +0900

    test10

---
+++ TestActivity.kt
@@ -0,0 +1,14 @@
+class TestActivity {
+
+    fun onCreate() {
+        println("TestActivity created")
+    }
+
+    fun loadData() {
+        println("Loading data...")
+    }
+
+    fun onDestroy() {
+        println("TestActivity destroyed")
+    }
+}
